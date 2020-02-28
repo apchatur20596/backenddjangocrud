20bdb4adfb9cae612d62c36db3a4022606331871
chatur20596 <chatur@usc.edu>
2020-02-28 05:25:33 -0800

    Create Employee api

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    emp_id = models.IntegerField()
+    location = models.CharField(max_length=200)
+    phone_number = models.IntegerField()
+    email_id = models.EmailField(max_length=100, unique=True)
+    joining_date = models.DateField(auto_now_add=True)
+    last_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
--- api/views.py
+++ api/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
 
 # Create your views here.
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List' : '/task-list',
+        'Detail View' : 'task-detail/<str:pk>/',
+        'Create' : '/createEmp/',
+        'Update' : 'task-update/<str:pk>/',
+        'Delete' : '/task-delete/<str:pk>/',
+    }
+
+    return Response(api_urls)
+
+@api_view(['POST'])
+def newEmployee(request):
+    print(request.data)
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
