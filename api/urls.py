f53f69765991384427ef5e72c0df859e9340958e
chatur20596 <chatur@usc.edu>
2020-02-28 05:37:35 -0800

    readEmp

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('',views.apiOverview, name="api-overview"),
     path('createEmp/', views.newEmployee, name="createEmp"),
     path('readAllEmp/',views.readAllEmp, name="readAllEmp"),
-
+    path('readEmp/<str:pk>', views.readEmployee, name="readEmp"),
 
 ]
--- api/views.py
+++ api/views.py
@@ -12,7 +12,7 @@ from .models import Task
 def apiOverview(request):
     api_urls = {
         'List' : '/readAllEmp',
-        'Detail View' : 'task-detail/<str:pk>/',
+        'Read' : 'readEmp/<str:pk>/',
         'Create' : '/createEmp/',
         'Update' : 'task-update/<str:pk>/',
         'Delete' : '/task-delete/<str:pk>/',
@@ -34,5 +34,11 @@ def newEmployee(request):
 def readAllEmp(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def readEmployee(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many=False)
     return Response(serializer.data)
 
